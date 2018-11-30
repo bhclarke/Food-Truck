19ae331ea86bb96f8a6f1bdc55fd4073532c1d2f
jkuwisc <43834143+jkuwisc@users.noreply.github.com>
2018-11-29 20:49:58 -0600

    added addtl methods for testing meal list view
    updated analyzeMealData and added getMealName -- methods used in the meal list view

--- Meal.java
+++ Meal.java
@@ -4,17 +4,16 @@ import java.util.Set;
 
 public class Meal extends FoodData {
 	
-	// a smaller list of foodItemList (from FoodData) that contains the food items making up
-	// the meal
-	private List<FoodItem> mealItems;
 	private FoodItem foodItem = null;
 	private HashMap<String, Double> nutrients = null;  // map of all nutrient values for a given food item
-	private String nutrientString = null;  // the string containing all nutrient labels and values
+	private String nutrientString = new String();  // the string containing all nutrient labels and values
 	private Set<String> nutrientLabels = null;  // all keys from our nutrient map
+	private String mealName = new String();
 	
+	/**
+	 * Default constructor -- foodItemList is defined in FoodData
+	 */
 	public Meal() {
-		mealItems = null;
-		
 	}
 	
 	/**
@@ -22,14 +21,15 @@ public class Meal extends FoodData {
 	 * for corresponding nutrition value
 	 * @return
 	 */
-	public String analyzeMealData() {
-		for (int i = 0; i < mealItems.size(); i++) {
-			foodItem = mealItems.get(i);  // look at each food item
+	public String analyzeMealData() {		
+		for (int i = 0; i < super.getAllFoodItems().size(); i++) {
+			foodItem = super.getAllFoodItems().get(i);  // look at each food item
 			nutrients = foodItem.getNutrients();  // get the nutrient map
 			nutrientLabels = nutrients.keySet();  // retrieve each nutrient and append its value to nutrientString
 			for (String label:nutrientLabels) {
-				nutrientString.concat(label + nutrients.get(label) + "\n");
+				nutrientString += label.toUpperCase() + ": " + nutrients.get(label) + "\n";
 			}
+			System.out.println("This is nutrient string: " + nutrientString);
 		}
 		return nutrientString;
 	}
@@ -39,7 +39,19 @@ public class Meal extends FoodData {
 	 * @param food
 	 */
 	public void removeFood(FoodItem food) {
-		mealItems.remove(food);
+		//mealItems.remove(food);
+		super.getAllFoodItems().remove(food);
+	}
+	
+	/**
+	 * Retrieves a display name for the meal
+	 * TODO: Figure out a good naming convention for meals -- right now, it just returns the first 4 chars of each FoodItem in foodItemList
+	 */
+	public String getMealName() {
+		for (FoodItem fi : super.getAllFoodItems()) {
+			mealName = mealName.concat(fi.getName().substring(0, 4));
+		}
+		return mealName;
 	}
 
 }
